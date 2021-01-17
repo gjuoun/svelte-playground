d61ca7eddcb81ee3d8a5ecac9e19df4762c0bb2f
John Guo <john.guo@affinio.com>
2021-01-16 21:51:23 -0400

    add basic examples

---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+  import Demo from "./demo.svelte";
+  let count = 0;
+
+  function handleClick() {
+    count += 1;
+  }
+</script>
+
+<h1>Hello {name}!</h1>
+<Demo />
+
+<button on:click={handleClick}>
+  Clicked {count} times
+</button>
+
+<style>
+  h1 {
+    color: green;
+  }
+</style>
