8b0da8e1ce7716ca48d5a4e6eb4f1c5beab71d91
Jun Guo <gjuoun@gmail.com>
2022-07-23 02:34:27 -0300

    add files

---
+++ vite-project-svelte/src/App.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+  import Button from "./components/Button.svelte";
+  import { onMount } from "svelte";
+  import SvelteMarkdown from "svelte-markdown";
+
+  let text = "";
+
+  let notes: string[] = [];
+</script>
+
+<main class="max-w-lg m-auto">
+  <div class="flex flex-col gap-y-2">
+    <h3 class="text-2xl font-bold">Create a note</h3>
+    <textarea
+      bind:value={text}
+      rows="12"
+      class="w-full border focus:outline-sky-500 p-2"
+    />
+
+    <SvelteMarkdown source={text} />
+
+    <div class="flex justify-end align-middle gap-x-2">
+      <Button color="red" text="Cancel" />
+      <Button
+        color="sky"
+        text="Create"
+        on:click={() => {
+          notes.push(text);
+          text = "";
+          console.log(notes);
+        }}
+      />
+    </div>
+  </div>
+</main>
---
+++ vite-project-svelte/src/components/Button.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import clsx from "clsx";
+
+  type Color = "red" | "sky";
+  export let color: Color = "sky";
+  let colorClass = "";
+  $: if (color === "sky") {
+    colorClass = "bg-sky-500 active:bg-sky-700";
+  } else {
+    colorClass = "bg-red-500 active:bg-red-700";
+  }
+
+  $: buttonClass = clsx("px-4 py-2 rounded-lg", colorClass, "text-white");
+  export let text: string;
+</script>
+
+<button on:click class={buttonClass}>{text}</button>
